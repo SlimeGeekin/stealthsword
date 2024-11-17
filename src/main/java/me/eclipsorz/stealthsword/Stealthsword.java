8f8ae511daa72d5cb771a0ebc4df2b4ae8cc0293
SlimeGeekin <155208007+SlimeGeekin@users.noreply.github.com>
2024-11-17 16:44:55 -0500

    start

---
+++ src/main/java/me/eclipsorz/stealthsword/Stealthsword.java
@@ -0,0 +1,17 @@
+package me.eclipsorz.stealthsword;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Stealthsword extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
